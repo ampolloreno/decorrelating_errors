4c283b9a636d17bce129f60aec1fa7ad37a7f33c
apolloreno <apolloreno@canopyfog.com>
2019-09-19 12:10:51 -0600

    removing code directory

---
+++ data/shared_code/pauli_channel_approximation.py
@@ -0,0 +1,250 @@
+import matplotlib
+matplotlib.use('Agg')
+from GRAPE import GRAPE, control_unitaries, adjoint
+import numpy as np
+import dill
+from functools import reduce
+import time as timemod
+from convex import (all_derivs, optimal_weights)
+from mpi4py import MPI
+import scipy
+import os
+from itertools import product
+
+COMM = MPI.COMM_WORLD
+
+I = np.eye(2)
+X = np.array([[0, 1], [1, 0]])
+Y = np.array([[0, -1.j], [1.j, 0]])
+Z = np.array([[1, 0], [0, -1]])
+PAULIS = [I, X, Y, Z]
+
+
+def generate_indices(num_points, order_desired):
+    num_indices = len(order_desired)
+    tuples = product(range(num_points), repeat=num_indices)
+    indices = [sum([num_points**(num_indices - 1 - order_desired[i]) * t[i] for i in range(num_indices)]) for t in tuples]
+    return indices
+
+
+def error_unitary(unitary, target):
+    """
+    Constructs the superoperator for the error unitary corresponding propagating forward by target,
+     and back by unitary.
+
+    :param np.array unitary: Control unitary realized in the computational basis.
+    :param np.array target: The desired unitary in the computational basis.
+    :return: The superoperator corresponding to the unitary postrotational error.
+    :rtype: numpy.array
+    """
+    error = adjoint(unitary).dot(target)
+    return np.kron(np.conj(error), error)
+
+
+def off_diagonal_projection(sop):
+    """
+    Computes a quantity proportional to the sum of the magnitudes of terms corresponding to non-stochastic evolution.
+
+    :param numpy.ndarry sop: Representation our operator as a superoperator, e.g. operating on vectorized density
+     matrices.
+    """
+    basis = PAULIS
+    for _ in range(int(np.log2(sop.shape[0]) / 2.0) - 1):
+        basis = [np.kron(base, pauli) for pauli in PAULIS for base in basis]
+    off_diagonal = 0
+    for i, base1 in enumerate(basis):
+        for j, base2 in enumerate(basis):
+            if i != j:
+                off_diagonal += np.abs(np.trace(adjoint(np.kron(base1, base2)).dot(sop))) ** 2
+    return off_diagonal
+
+
+def error_on_controls(ambient_hamiltonian, control_hamiltonians, controls, dt,
+                      target_operator):
+    unitary = reduce(lambda a, b: a.dot(b),
+                     control_unitaries(ambient_hamiltonian, control_hamiltonians,
+                                       controls, dt))
+    error = error_unitary(unitary, target_operator)
+    return error
+
+
+class PCA(object):
+    """Class to perform Pauli Channel Approximations- i.e. pick out weights for families of controls that make them look
+     most like Pauli Channels."""
+
+    def __init__(self, num_controls, ambient_hamiltonian, control_hamiltonians, target_operator,
+                 num_steps, time, threshold, detunings, dirname):
+        COMM = MPI.COMM_WORLD
+        self.seed = 138 + 500
+        np.random.seed(self.seed)
+        self.start = timemod.time()
+        controlset = []
+        dt = time / num_steps
+        self.num_controls = num_controls
+        for i in range(num_controls):
+            if (i % COMM.size) != COMM.rank:
+                continue
+            print(f'{COMM.rank} doing control {i}')
+            np.random.seed(self.seed + i)
+            random_detunings = []
+            for detuning in detunings:
+                random_detunings.append((detuning[0], detuning[1]))
+            import sys
+            sys.stdout.flush()
+            result = GRAPE(ambient_hamiltonian, control_hamiltonians, target_operator,
+                           num_steps, time, threshold, random_detunings, i)
+            controlset.append(result.reshape(-1, len(control_hamiltonians)))
+            dill.dump(result.reshape(-1, len(control_hamiltonians)), open(os.path.join(dirname, f'control_{i}'), 'wb'))
+        print(f'controlset has {len(controlset)} members')
+        controlset = MPI.COMM_WORLD.gather(controlset, root=0)
+        if COMM.rank == 0:
+            controlset = [item for sublist in controlset for item in sublist]
+            self.controlset = controlset
+        self.detunings = detunings
+        self.target_operator = target_operator
+        self.dt = dt
+        self.ambient_hamiltonian = ambient_hamiltonian
+        self.control_hamiltonians = control_hamiltonians
+
+    def assign_weights(self):
+        derivs = all_derivs(self.controlset, self.target_operator, self.control_hamiltonians, self.ambient_hamiltonian,
+                            self.dt, 1)
+        weights_0 = optimal_weights(derivs[:1])
+        weights = optimal_weights(derivs)
+        self.derivs = derivs
+        self.weights = weights
+        self.weights_0 = weights_0
+
+
+def compute_dpn_and_fid(data):
+    from copy import deepcopy
+    import sys
+    controlset, ambient_hamiltonian0, combo, dt, control_hamiltonians, target_operator, probs = data
+    sys.stdout.flush()
+    fidelities = []
+    projs = []
+    sops = []
+    controlset_unitaries = []
+    for controls in controlset:
+        newcontrols = deepcopy(controls)
+        ambient_hamiltonian = [deepcopy(ah).astype("complex") for ah in ambient_hamiltonian0]
+        for cnum, value in enumerate(combo):
+            cnum -= len(ambient_hamiltonian0)
+            if cnum >= 0:
+                newcontrols[:, cnum] = newcontrols[:, cnum] * (1 + value)
+            if cnum < 0:
+                if cnum == -1:
+                    import sys
+                    sys.stdout.flush()
+                    ambient_hamiltonian[cnum] *= 1 + float(value)
+                else:
+                    ambient_hamiltonian[cnum] *= float(value)
+        step_unitaries = control_unitaries(ambient_hamiltonian,
+                                           control_hamiltonians, newcontrols,
+                                           dt)
+        unitary = reduce(lambda a, b: a.dot(b), step_unitaries)
+        sop = error_unitary(unitary, target_operator)
+        sops.append(sop)
+        projs.append(off_diagonal_projection(sop))
+        controlset_unitaries.append(unitary)
+        fidelity = np.trace(adjoint(target_operator).dot(unitary))/target_operator.shape[0]
+        fidelity *= np.conj(fidelity)
+        fidelities.append(fidelity)
+    # print(sops)
+    # print(probs)
+    # print([prob * sops[i] for i, prob in enumerate(probs)])
+    avg_sop = reduce(lambda a, b: a + b, [prob * sops[i] for i, prob in enumerate(probs)])
+    balanced = reduce(lambda a, b: a + b,
+                      [probs[i] * np.kron(np.conj(unitary), unitary) for i, unitary in
+                       enumerate(controlset_unitaries)])
+    balanced_fidelity = np.trace(
+        adjoint(balanced).dot(np.kron(np.conj(target_operator), target_operator)))/(target_operator.shape[0])**2
+
+    fidelities.append(balanced_fidelity)
+    projs.append(off_diagonal_projection(avg_sop))
+
+    fidelities = np.array(fidelities).T
+    projs = np.array(projs).T
+
+    return projs, fidelities
+
+
+def gen_1q():
+    COMM = MPI.COMM_WORLD
+    I = np.eye(2)
+    X = np.array([[0, 1], [1, 0]])
+    Y = np.array([[0, -1.j], [1.j, 0]])
+    Z = np.array([[1, 0], [0, -1]])
+    ambient_hamiltonian = [Z]
+    control_hamiltonians = [X, Y]
+    detunings = [(1E-3, 1), (1E-3,  2)]
+    target_operator = scipy.linalg.sqrtm(X)
+    time = np.pi
+    num_steps = 10
+    threshold = 1 - .001
+    num_controls = 50
+    i = 0
+    while os.path.exists("pickled_controls%s.pkl" % i):
+        i += 1
+    dirname = f'controls_{i}'
+    if COMM.rank == 0:
+        if not os.path.exists(dirname):
+            os.mkdir(dirname)
+    pca = PCA(num_controls, ambient_hamiltonian, control_hamiltonians, target_operator,
+              num_steps, time, threshold, detunings, dirname)
+    if COMM.rank == 0:
+        fh = open(os.path.join(dirname, "just_pickled_controls%s.pkl" % i), 'wb')
+        dill.dump(pca, fh)
+        pca.assign_weights()
+        fh.close()
+        fh = open(os.path.join(dirname, "pickled_controls%s.pkl" % i), "wb")
+        dill.dump(pca, fh)
+        fh.close()
+
+
+def gen_2q():
+    COMM = MPI.COMM_WORLD
+    I = np.eye(2)
+    X = np.array([[0, 1], [1, 0]])
+    Y = np.array([[0, -1.j], [1.j, 0]])
+    Z = np.array([[1, 0], [0, -1]])
+    IZ = np.kron(I, Z)
+    ZI = np.kron(Z, I)
+    XI = np.kron(X, I)
+    IX = np.kron(I, X)
+    IY = np.kron(I, Y)
+    YI = np.kron(Y, I)
+    ZZ = np.kron(Z, Z)
+    XX = np.kron(X, X)
+    YY = np.kron(Y, Y)
+    entangle_ZZ = np.array([[1, 0, 0, 0], [0, 1.j, 0, 0], [0, 0, 1.j, 0], [0, 0, 0, 1]])
+    iswap = np.array([[1, 0, 0, 0], [0, 0, -1.j, 0], [0, -1.j, 0, 0], [0, 0, 0, 1]])
+    ambient_hamiltonian = [IZ, ZI, (XX + YY)/10.]
+    control_hamiltonians = [IX, IY, XI, YI]
+    detunings = [(.001, 1), (.001, 1), (0, 1), (.001, 2), (.001, 2)]
+    target_operator = iswap
+    time = 5/2*np.pi
+    num_steps = 40
+    threshold = 1 - .001
+    num_controls = 500
+    i = 0
+    while os.path.exists("pickled_controls%s.pkl" % i) or os.path.exists(f'controls_{i}'):
+        i += 1
+    dirname = f'controls_{i}'
+    if COMM.rank == 0:
+        if not os.path.exists(dirname):
+            os.mkdir(dirname)
+    pca = PCA(num_controls, ambient_hamiltonian, control_hamiltonians, target_operator,
+              num_steps, time, threshold, detunings, dirname)
+    if COMM.rank == 0:
+        fh = open(os.path.join(dirname, "just_pickled_controls%s.pkl" % i), 'wb')
+        dill.dump(pca, fh)
+        pca.assign_weights()
+        fh.close()
+        fh = open(os.path.join(dirname, "pickled_controls%s.pkl" % i), "wb")
+        dill.dump(pca, fh)
+        fh.close()
+
+
+if __name__ == '__main__':
+    gen_2q()
---
+++ data/two_qubit_numeric/2q_dnorm.py
@@ -0,0 +1,170 @@
+import os
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
+os.environ['GOTO_NUM_THREADS'] = '1'
+os.environ['OMP_NUM_THREADS'] = '1'
+os.environ['NUMEXPR_NUM_THREADS'] = '1'
+os.environ['VECLIB_MAXIMUM_THREADS'] = '1'
+
+from pauli_channel_approximation import PCA
+from time import time
+
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+import numpy as np
+import scipy as sp
+import numpy.linalg as lin
+import scipy.linalg as slin
+
+import os; os.environ['PYGSTI_BACKCOMPAT_WARNING'] = '0'
+import pygsti 
+
+import pygsti
+import numpy as np
+import itertools
+from functools import reduce
+
+multi_kron = lambda *a: reduce(np.kron, a)
+
+def change_basis(process, from_basis, to_basis):
+    """
+    PyGSTi 'std' basis is *row stacked* rather than column stacked, so need to defined a 'col' basis.
+    """
+    allowed_bases = ['col', 'std', 'gm', 'pp', 'qsim']
+    if not ((from_basis in allowed_bases) or (to_basis in allowed_bases)):
+        raise ValueError("Allowed bases are 'col', 'std', 'gm', 'pp', 'qsim' ")
+
+    n_qubits = int(np.log(len(process)) / np.log(4))
+    if from_basis == 'col' or to_basis == 'col':
+
+        if len(process) != 4**n_qubits:
+            raise ValueError('The dimension of the process matrix must be a power of 4.')
+        def matrix_unit(n,i,j):
+            unit = np.zeros([n,n], dtype='complex')
+            unit[i,j] = 1.
+            return unit
+        col_stacked_matrix_units = [matrix_unit(2**n_qubits, i, j) for j in range(2**n_qubits) for i in range(2**n_qubits)]
+
+        from pygsti.objects import ExplicitBasis
+        col = ExplicitBasis(col_stacked_matrix_units, ["myElement%d" % i for i in range(4**n_qubits)],
+                                     name='col', longname='Column=-Stacked')
+        if from_basis == 'col':
+            from_basis = col
+        if to_basis == 'col':
+            to_basis = col
+
+    if from_basis == 'qsim' or to_basis == 'qsim':
+        n_qubits = int(np.log(len(process))/np.log(4))
+        if len(process) != 4**n_qubits:
+            raise ValueError('The dimension of the process matrix must be a power of 4.')
+        sig0q = np.array([[1., 0], [0, 0]], dtype='complex')
+        sigXq = np.array([[0, 1], [1, 0]], dtype='complex')/np.sqrt(2)
+        sigYq = np.array([[0, -1], [1, 0]], dtype='complex') * 1.j / np.sqrt(2.)
+        sig1q = np.array([[0, 0], [0, 1]], dtype='complex')
+        qbasis = itertools.product([sig0q, sigXq, sigYq, sig1q], repeat=n_qubits)
+        qbasis = [multi_kron(*x) for x in qbasis]
+
+        from pygsti.objects import ExplicitBasis
+        qsim = ExplicitBasis(qbasis, ["myElement%d" % i for i in range(4**n_qubits)],
+                                     name='qsim', longname='Quantumsim')
+        if from_basis == 'qsim':
+            from_basis = qsim
+        if to_basis == 'qsim':
+            to_basis = qsim
+
+    return pygsti.tools.basistools.change_basis(process, from_basis, to_basis)
+
+def get_unitary(data, c_ind, epsilon1, epsilon2, delta1, delta2):
+    u = np.eye(4, dtype='complex')
+    
+    a = epsilon1 * data.ambient_hamiltonian[0]
+    b = epsilon2 * data.ambient_hamiltonian[1]
+    c = data.ambient_hamiltonian[2] 
+    h0 = a+b+c
+    
+    for t_ind in range(500):
+        d = (1+delta2) * data.controlset[c_ind][t_ind][0] * data.control_hamiltonians[0]
+        e = (1+delta2) * data.controlset[c_ind][t_ind][1] * data.control_hamiltonians[1] 
+        f = (1+delta1) * data.controlset[c_ind][t_ind][2] * data.control_hamiltonians[2] 
+        g = (1+delta1) * data.controlset[c_ind][t_ind][3] * data.control_hamiltonians[3] 
+        ham = h0 + d + e + f + g
+        u = np.dot(slin.expm(-1.j*ham*data.dt), u)
+    
+    return u        
+
+iSWAP = np.zeros([4,4], dtype='complex')
+iSWAP[0,0] = 1
+iSWAP[3,3] = 1
+iSWAP[1,2] = -1.j
+iSWAP[2,1] = -1.j
+piSWAP = np.kron(iSWAP.conj(), iSWAP)
+piSWAP = change_basis(piSWAP, 'col', 'pp')
+
+
+if rank == 0:
+    import dill
+    with open('0_pickled_controls106.pkl', 'rb') as f:
+        data0 = dill.load(f)
+    with open('1_pickled_controls106.pkl', 'rb') as f:
+        data1 = dill.load(f)
+
+    with open('./figures/0_epsilon.dat', 'rb') as f:
+        ep0 = dill.load(f)
+    with open('./figures/epsilon.dat', 'rb') as f:
+        ep1 = dill.load(f)
+else:
+    data0 = None
+    data1 = None
+    ep0 = None
+    ep1 = None
+
+
+def split(n, a):
+    k, m = divmod(len(a), n)
+    return np.array(list(a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n)))
+
+
+data0 = comm.bcast(data0, root=0)
+data1 = comm.bcast(data1, root=0)
+ep0 = comm.bcast(ep0, root=0)
+ep1 = comm.bcast(ep1, root=0)
+
+
+
+my_points = split(size, np.arange(data0.num_controls))[rank]
+my_values = []
+
+t0 = time()
+num_points = len(my_points)
+for p_ind, point in enumerate(my_points):
+    t1 = time()
+    ed = []
+    for val in ep0['vals']:
+        u = get_unitary(data1, point, val,val,0,0)
+        pu = change_basis(np.kron(u.conj(), u), 'col', 'pp')
+        ed += [pygsti.tools.optools.diamonddist(piSWAP, pu, 'pp')/2.]
+        # ed += [0]
+    my_values += [ed]
+    delta_t = np.round(time() - t1)
+    delta_T = np.round(time() - t0)
+    print(f"Node {rank} finished {p_ind+1} of {num_points} in {delta_T} seconds. Remaining: {delta_T*(num_points-p_ind-1)/(p_ind+1)}. Min dnorm: {np.min(ed)}.")
+
+all_values = comm.gather(my_values, root = 0)
+
+
+if rank == 0:
+    print(np.array(all_values).shape)
+    all_values = [inner for outer in all_values for inner in outer]    
+    data = {ind: all_values[ind] for ind in range(data0.num_controls)}
+    data['epsilons'] = ep0['vals']
+    with open('2q_dnorms.dat', 'wb') as f:
+        dill.dump(data, f)
+
+
+
+
+
+
+
